c3667038ce651acbe281126381c2f30bd93b92aa
Ruthy Levi <rlevi@stevens.edu>
2019-09-23 17:46:51 -0400

    Working on functions

--- Lab 3/index.js
+++ Lab 3/index.js
@@ -2,9 +2,10 @@ const axios = require('axios');
 
 async function getPersonById(id){
     try {
+        console.log('calling');
         const { data } = await axios.get('https://gist.githubusercontent.com/robherley/5112d73f5c69a632ef3ae9b7b3073f78/raw/24a7e1453e65a26a8aa12cd0fb266ed9679816aa/people.json');
-        return (data[id]);
-        // return data // this will be the array of people
+        console.log(data[id-1]); //to make sure it works
+        return data[id-1]; 
       } catch (error) {
           console.error(error);
       }
